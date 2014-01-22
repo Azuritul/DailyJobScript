126a4427fc9cedf3b7389771a0c74533f32868b5
Azuritul <azuritul@gmail.com>
2014-01-22 16:54:21 +0800

    Made the program accept arguments instead of using prompt

--- groupby.py
+++ groupby.py
@@ -1,4 +1,4 @@
-__author__ = 'yungchou.wu'
+__author__ = 'Azuritul Wu'
 import fnmatch
 import os
 import shutil
@@ -23,23 +23,51 @@ def scrap_last_name(file):
         split_name = file.split('_')
         return split_name[2][0:6]
 
+def create_if_no_folder(folder_name, context_path):
+    full_path = os.path.join(context_path, folder_name)
+    if not os.path.exists(full_path) and folder_name != '':
+        logging.info('folder not exist, creating ... ' + full_path)
+        os.mkdir(full_path)
+    return full_path
+
+def decide_folder_name(filename, groupby):
+    if groupby == 'month':
+        return scrap_last_name(filename)
+    elif groupby == 'name':
+        return scrap_middle_name(filename)
+
+
+def process_file(filename, kind, context_path):
+    folder_name = decide_folder_name(filename, kind)
+    destination_folder = create_if_no_folder(folder_name, context_path)
+    src_file = os.path.join(context_path, filename)
+    logging.info('Moving ' + filename + ' to ' + os.path.abspath(destination_folder))
+    shutil.move(src_file, os.path.abspath(destination_folder))
+
+def is_valid_file(filename):
+    full_path = os.path.abspath(filename)
+    if os.path.isfile(full_path) and fnmatch.fnmatch(filename, 'data_*.xml'):
+        return True
+    return False
+
+
 def group_by_filename(dir, kind):
     file_list = os.listdir(dir)
-    folder_name = ''
     for filename in file_list:
+        if os.path.isdir(filename):
+            context_path = dir  + filename
+        else:
+            context_path = dir
         if fnmatch.fnmatch(filename, 'data_*.xml'):
-            if kind == 'month':
-                folder_name = scrap_last_name(filename)
-            elif kind == 'name':
-                folder_name = scrap_middle_name(filename)
+            process_file(filename, kind, context_path)
 
-            if not os.path.exists(folder_name) and folder_name != '':
-                os.mkdir(folder_name)
-
-            src_file = os.path.join(os.getcwd(), filename)
-            dest_file = os.path.join(os.getcwd(), folder_name)
-            logging.info('Moving ' + filename + ' to ' + dest_file)
-            shutil.move(src_file, dest_file)
+def main(arguments):
+    target = arguments[1]
+    if arguments[0] == 'month' or arguments[0] == 'name':
+        group_by_filename(target, arguments[0])
+    else:
+        logging.info('Do nothing')
+        print('Please provide correct options: [month | name]')
 
 curr_ver = sys.version_info
 if curr_ver >= (3,2):
@@ -47,8 +75,8 @@ if curr_ver >= (3,2):
 else:
     prompt = raw_input
 
-logging.basicConfig(format='%(asctime)s %(message)s',filename='groupby.log',level=logging.INFO)
-kind = prompt("by month or by name? ")
-group_by_filename('.', kind)
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(message)s',filename='groupby.log',level=logging.INFO)
+    main(sys.argv[1:])
 
 
