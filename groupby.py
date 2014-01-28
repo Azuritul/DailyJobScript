85836b3e3fa99bad5e30133d99df33227cbf1fdc
Azuritul <azuritul@gmail.com>
2014-01-28 14:03:36 +0800

    Reformat code.

--- groupby.py
+++ groupby.py
@@ -5,6 +5,7 @@ import shutil
 import logging
 import sys
 
+
 def scrap_middle_name(file):
     """
     Getting folder name from the splitted file_name
@@ -13,6 +14,7 @@ def scrap_middle_name(file):
     if file.startswith('data_') and file.endswith('.xml') and file.count('_') == 2:
         split_name = file.split('_')
         return split_name[1]
+
 
 def scrap_last_name(file):
     """
@@ -23,12 +25,14 @@ def scrap_last_name(file):
         split_name = file.split('_')
         return split_name[2][0:6]
 
+
 def create_if_no_folder(folder_name, context_path):
     full_path = os.path.join(context_path, folder_name)
     if not os.path.exists(full_path) and folder_name != '':
         logging.info('folder not exist, creating ... ' + full_path)
         os.mkdir(full_path)
     return full_path
+
 
 def decide_folder_name(filename, groupby):
     if groupby == 'month':
@@ -44,6 +48,7 @@ def process_file(filename, kind, context_path):
     logging.info('Moving ' + filename + ' to ' + os.path.abspath(destination_folder))
     shutil.move(src_file, os.path.abspath(destination_folder))
 
+
 def is_valid_file(filename):
     full_path = os.path.abspath(filename)
     if os.path.isfile(full_path) and fnmatch.fnmatch(filename, VALID_FILE_NAME):
@@ -51,15 +56,16 @@ def is_valid_file(filename):
     return False
 
 
-def group_by_filename(dir, kind):
-    file_list = os.listdir(dir)
+def group_by_filename(folder, kind):
+    file_list = os.listdir(folder)
     for filename in file_list:
         if os.path.isdir(filename):
-            context_path = dir  + filename
+            context_path = folder + filename
         else:
-            context_path = dir
+            context_path = folder
         if fnmatch.fnmatch(filename, VALID_FILE_NAME):
             process_file(filename, kind, context_path)
+
 
 def main(arguments):
     target = arguments[1]
@@ -69,15 +75,14 @@ def main(arguments):
         logging.info('Do nothing')
         print('Please provide correct options: [month | name]')
 
+
 curr_ver = sys.version_info
-if curr_ver >= (3,2):
+if curr_ver >= (3, 2):
     prompt = input
 else:
     prompt = raw_input
 
 VALID_FILE_NAME = 'data_*_*.xml'
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s %(message)s',filename='groupby.log',level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s %(message)s', filename='groupby.log', level=logging.INFO)
     main(sys.argv[1:])
-
-
