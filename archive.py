7895920f24091777a870660242bd046606c2b170
Azuritul <azuritul@gmail.com>
2014-02-14 15:38:04 +0800

    Modified so that the file can correctly archive file in last month.

--- archive.py
+++ archive.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+import os
 import sys
 
 __author__ = 'Azuritul Wu'
 
 import tarfile
 import logging
+import sys
 from contextlib import closing
 from datetime import date
 
@@ -14,28 +16,45 @@ from datetime import date
     AUDIT_20140206_050820.csv
     tar -czvf AUDIT_yyyyMMDD_*.csv AUDIT_yyyy_JAN.tar.gz
 """
-def make_tarfile(output_filename, source_dir):
-    with closing(tarfile.open(output_filename, "w:gz")) as tar:
-        tar.add(source_dir)
 
-def gen_tar_name():
-    month_arr = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+
+def do_tar(target_dir):
+    month_str_arr = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+    month_digit_arr = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
     curr_month = date.today().strftime('%b')
     curr_year = date.today().strftime('%Y')
     filename_year = curr_year
-    pre_month = month_arr.index(curr_month.upper()) - 1
+    pre_month_idx = month_str_arr.index(curr_month.upper()) - 1
 
     if curr_month == 0:
-        pre_month = 11
+        pre_month_idx = 11
         filename_year = int(curr_year) - 1
 
-    print('previous month is : ' , month_arr[pre_month] )
-    return 'AUDIT_' + str(filename_year) + '_' + month_arr[pre_month] + '.tar'
+    tar_name = 'AUDIT_' + str(filename_year) + '_' + month_str_arr[pre_month_idx] + '.tar.gz'
+
+    matched_file_name = str(filename_year) + str(month_digit_arr[pre_month_idx])
+
+    with closing(tarfile.open(tar_name, "w:gz")) as tar:
+        file_list = [f for f in os.listdir(target_dir) if
+                     f.count('_') > 1 and f.split('_')[1].find(matched_file_name) != -1]
+
+        logging.info('Start tar operation ...')
+
+        for pending in file_list:
+            logging.info('Tarring ' + pending)
+            tar.add(pending)
+
+        logging.info('Finished tar operation, totally tar ' + str(len(file_list)) + ' files')
+
 
 def main(arguments):
     target_folder = arguments[1]
-    make_tarfile(gen_tar_name(), target_folder)
+    do_tar(target_folder)
+
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(message)s', filename='archive_audit.log', level=logging.INFO)
+    if len(sys.argv) < 2:
+        print('Please provide tar folder.')
+        sys.exit()
     main(sys.argv[0:])
