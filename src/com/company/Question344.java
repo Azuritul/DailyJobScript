71185a455d3952e9db4528474d36c0bfc2762f1f
wu <y-wu@royalgate.co.jp>
2016-05-06 18:18:28 +0900

    Workable version

--- src/com/company/Question344.java
+++ src/com/company/Question344.java
@@ -1,28 +1,50 @@
 package com.company;
 
-/**
- * Created by y-wu on 2016/05/06.
- */
-public class Question344 {
-
-    /*
+/*
     Write a function that takes a string as input and returns the string reversed.
 
     Example:
     Given s = "hello", return "olleh".
 
     Subscribe to see which companies asked this question
-    */
+*/
+public class Question344 {
+
     public static void main(String[] args) {
         String target = "hello";
-        reverseString(target); //olleh
+        System.out.println(reverseString(target)); //should be "olleh"
+
+        target = "welcome";
+        System.out.println(reverseString(target)); //should be "emoclew"
+
+        target = "system";
+        System.out.println(reverseString(target)); //should be "metsys"
     }
 
     public static String reverseString(String original){
-        //stop condition
-        //if length odd
-        //if length even
+        int swapCount = original.length() / 2;
+        char[] arr = original.toCharArray();
+        int backIndex = original.length() - 1;
+        for(int i = 0; i < swapCount; i++) {
+            swap(arr, i, backIndex);
+            backIndex--;
+        }
 
+        return printArrayToString(arr);
+    }
+
+    private static String printArrayToString(char[] array) {
+        StringBuilder builder = new StringBuilder();
+        for (char c : array) {
+            builder.append(c);
+        }
+        return builder.toString();
+    }
+
+    public static char[] swap(char[] original, int front, int back){
+        char temp = original[front];
+        original[front] = original[back];
+        original[back] = temp;
         return original;
     }
 
