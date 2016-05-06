52b8b5143e3321f8b8433bfaa43edf73fb933ab4
wu <y-wu@royalgate.co.jp>
2016-05-06 15:04:07 +0900

    Add 344.

---
+++ src/com/company/Question344.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * Created by y-wu on 2016/05/06.
+ */
+public class Question344 {
+
+    /*
+    Write a function that takes a string as input and returns the string reversed.
+
+    Example:
+    Given s = "hello", return "olleh".
+
+    Subscribe to see which companies asked this question
+    */
+    public static void main(String[] args) {
+        String target = "hello";
+        reverseString(target); //olleh
+    }
+
+    public static String reverseString(String original){
+        //stop condition
+        //if length odd
+        //if length even
+
+        return original;
+    }
+
+}
